ddda4702ee35880c5eedba1d82e370cababc5058
dougvader <30653340+dougvader@users.noreply.github.com>
2022-10-25 14:13:46 +1000

    Commit 2
    Added 2021 projects

---
+++ 2021/FindDeadPlaylist/FindDeadPlaylist/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace FindDeadPlaylist
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.select_playlist_label = new System.Windows.Forms.Label();
+            this.playlist_textbox = new System.Windows.Forms.RichTextBox();
+            this.select_playlist_button = new System.Windows.Forms.Button();
+            this.output_location_button = new System.Windows.Forms.Button();
+            this.output_textbox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.search_location_button = new System.Windows.Forms.Button();
+            this.search_textbox = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.go_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // select_playlist_label
+            // 
+            this.select_playlist_label.AutoSize = true;
+            this.select_playlist_label.Location = new System.Drawing.Point(55, 19);
+            this.select_playlist_label.Name = "select_playlist_label";
+            this.select_playlist_label.Size = new System.Drawing.Size(65, 15);
+            this.select_playlist_label.TabIndex = 0;
+            this.select_playlist_label.Text = "Playlist File";
+            // 
+            // playlist_textbox
+            // 
+            this.playlist_textbox.Location = new System.Drawing.Point(126, 15);
+            this.playlist_textbox.Name = "playlist_textbox";
+            this.playlist_textbox.Size = new System.Drawing.Size(239, 24);
+            this.playlist_textbox.TabIndex = 1;
+            this.playlist_textbox.Text = "select playlyist file ->";
+            // 
+            // select_playlist_button
+            // 
+            this.select_playlist_button.Location = new System.Drawing.Point(371, 15);
+            this.select_playlist_button.Name = "select_playlist_button";
+            this.select_playlist_button.Size = new System.Drawing.Size(26, 23);
+            this.select_playlist_button.TabIndex = 2;
+            this.select_playlist_button.Text = "...";
+            this.select_playlist_button.UseVisualStyleBackColor = true;
+            this.select_playlist_button.Click += new System.EventHandler(this.select_playlist_button_Click);
+            // 
+            // output_location_button
+            // 
+            this.output_location_button.Location = new System.Drawing.Point(371, 54);
+            this.output_location_button.Name = "output_location_button";
+            this.output_location_button.Size = new System.Drawing.Size(26, 23);
+            this.output_location_button.TabIndex = 5;
+            this.output_location_button.Text = "...";
+            this.output_location_button.UseVisualStyleBackColor = true;
+            this.output_location_button.Click += new System.EventHandler(this.output_location_button_Click);
+            // 
+            // output_textbox
+            // 
+            this.output_textbox.Location = new System.Drawing.Point(126, 54);
+            this.output_textbox.Name = "output_textbox";
+            this.output_textbox.Size = new System.Drawing.Size(239, 24);
+            this.output_textbox.TabIndex = 4;
+            this.output_textbox.Text = "choose output location ->";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Output Destination";
+            // 
+            // search_location_button
+            // 
+            this.search_location_button.Location = new System.Drawing.Point(371, 94);
+            this.search_location_button.Name = "search_location_button";
+            this.search_location_button.Size = new System.Drawing.Size(26, 23);
+            this.search_location_button.TabIndex = 8;
+            this.search_location_button.Text = "...";
+            this.search_location_button.UseVisualStyleBackColor = true;
+            this.search_location_button.Click += new System.EventHandler(this.search_location_button_Click);
+            // 
+            // search_textbox
+            // 
+            this.search_textbox.Location = new System.Drawing.Point(126, 94);
+            this.search_textbox.Name = "search_textbox";
+            this.search_textbox.Size = new System.Drawing.Size(239, 24);
+            this.search_textbox.TabIndex = 7;
+            this.search_textbox.Text = "choose search directory ->";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Search Location";
+            // 
+            // go_button
+            // 
+            this.go_button.Location = new System.Drawing.Point(126, 142);
+            this.go_button.Name = "go_button";
+            this.go_button.Size = new System.Drawing.Size(239, 23);
+            this.go_button.TabIndex = 9;
+            this.go_button.Text = "Go!";
+            this.go_button.UseVisualStyleBackColor = true;
+            this.go_button.Click += new System.EventHandler(this.go_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 193);
+            this.Controls.Add(this.go_button);
+            this.Controls.Add(this.search_location_button);
+            this.Controls.Add(this.search_textbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.output_location_button);
+            this.Controls.Add(this.output_textbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.select_playlist_button);
+            this.Controls.Add(this.playlist_textbox);
+            this.Controls.Add(this.select_playlist_label);
+            this.Name = "Form1";
+            this.Text = "Find Dead Playlist";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label select_playlist_label;
+        private System.Windows.Forms.RichTextBox playlist_textbox;
+        private System.Windows.Forms.Button select_playlist_button;
+        private System.Windows.Forms.Button output_location_button;
+        private System.Windows.Forms.RichTextBox output_textbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button search_location_button;
+        private System.Windows.Forms.RichTextBox search_textbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button go_button;
+    }
+}
+
---
+++ 2021/FindDeadPlaylist/FindDeadPlaylist/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Security;
+using System.Security.Permissions;
+
+namespace FindDeadPlaylist
+{
+    public partial class Form1 : Form
+    {
+        private string playlist_location;
+        private string output_location;
+        private string search_location;
+        private List<string> filenames;
+        private const string search_pattern = "*.mp3";
+        private const SearchOption search_option = SearchOption.AllDirectories;
+        private EnumerationOptions e_options = new EnumerationOptions()
+        {
+            IgnoreInaccessible = true,
+            AttributesToSkip = FileAttributes.Hidden | FileAttributes.System,
+            RecurseSubdirectories = true
+        };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void select_playlist_button_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog open_playlist_explorer_window = new OpenFileDialog())
+            {
+                open_playlist_explorer_window.Title = "Choose .m3u file";
+                open_playlist_explorer_window.InitialDirectory = @"c:\\";
+                open_playlist_explorer_window.DefaultExt = "m3u";
+                open_playlist_explorer_window.Filter = "m3u files (*.m3u)|*.m3u|All files (*.*)|*.*";
+                open_playlist_explorer_window.FilterIndex = 2;
+                open_playlist_explorer_window.RestoreDirectory = true;
+
+                if (open_playlist_explorer_window.ShowDialog() == DialogResult.OK)
+                {
+                    playlist_location = open_playlist_explorer_window.FileName;
+                    playlist_textbox.Text = playlist_location;
+                }
+            }
+        }
+
+        private void output_location_button_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog output_destination_explorer_window = new FolderBrowserDialog())
+            {
+                output_destination_explorer_window.Description = "Choose output folder";
+                DialogResult result = output_destination_explorer_window.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(output_destination_explorer_window.SelectedPath))
+                {
+                    output_location = output_destination_explorer_window.SelectedPath;
+                    output_textbox.Text = output_location;
+                }
+            }
+        }
+
+        private void search_location_button_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog search_location_explorer_window = new FolderBrowserDialog())
+            {
+                search_location_explorer_window.Description = "Choose output folder";
+                DialogResult result = search_location_explorer_window.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(search_location_explorer_window.SelectedPath))
+                {
+                    search_location = search_location_explorer_window.SelectedPath;
+                    search_textbox.Text = search_location;
+                }
+            }
+        }
+
+        private void go_button_Click(object sender, EventArgs e)
+        {
+            if (search_textbox.Text != null && playlist_textbox.Text != null && output_textbox.Text != null)
+            {
+                var new_directory = System.IO.Directory.CreateDirectory(output_location);
+                //open playlist file and get file names
+                var playlist = System.IO.File.ReadAllLines(playlist_location);
+                filenames = new List<string>(playlist.Length);
+                foreach (string file in playlist)  
+                {
+                    filenames.Add(file);
+                }
+
+                //for each track in playlist
+                //foreach (string playlist_file in filenames)
+                //{
+                try
+                {
+                    // conduct search to find file that matches missing file name
+                    Debug.WriteLine("Conducting search...");                      
+                    var files_found = SafeWalk.EnumerateFiles(search_location, search_pattern, search_option, filenames);
+                    var unable_to_find_list = filenames;
+
+                    // for each file found, copy that file to the output folder
+                    foreach (var file in files_found)
+                    {
+                        Debug.WriteLine("adding track to folder " + file);
+                        unable_to_find_list.Remove(file);
+                        System.IO.File.Copy(file, Path.Combine(output_location, Path.GetFileName(file)), true);
+                    }
+                    // create a new playlist file for each file that wasn't found
+                    var unable_to_find_text = System.IO.File.CreateText(Name = "unable_to_find");
+
+
+                    // for each file it could not find, add it to "unable_to_find" text file
+                    foreach (var file in unable_to_find_list)
+                    {
+                        unable_to_find_text.WriteLine(file);
+                    }
+                    unable_to_find_text.Close();
+
+                    Debug.WriteLine(files_found.Count() + " dead songs found and added to output foler.");
+                    Debug.WriteLine(unable_to_find_list.Count() + " dead songs not found.");
+
+
+                } catch(Exception ex)
+                {
+                    //continue;
+                }                       
+                //}
+            } else { 
+                // Show popup "Please enter terms for each field"
+            }
+        }
+       
+        public static int Compute(string s, string t)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                if (string.IsNullOrEmpty(t))
+                    return 0;
+                return t.Length;
+            }
+
+            if (string.IsNullOrEmpty(t))
+            {
+                return s.Length;
+            }
+
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            // initialize the top and right of the table to 0, 1, 2, ...
+            for (int i = 0; i <= n; d[i, 0] = i++) ;
+            for (int j = 1; j <= m; d[0, j] = j++) ;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+                    int min1 = d[i - 1, j] + 1;
+                    int min2 = d[i, j - 1] + 1;
+                    int min3 = d[i - 1, j - 1] + cost;
+                    d[i, j] = Math.Min(Math.Min(min1, min2), min3);
+                }
+            }
+            return d[n, m];
+        }
+    }
+    public static class SafeWalk
+    {
+        public static IEnumerable<string> EnumerateFiles(string path, string searchPattern, SearchOption searchOpt, List<string> dead_tracks)
+        {
+            List<string> files = new List<string>() { };
+
+            try
+            {
+                Parallel.ForEach(Directory.EnumerateFiles(path, searchPattern, searchOpt), file =>
+                {
+                    Parallel.For(0, dead_tracks.Count, index =>
+                    {
+                        if (dead_tracks[index].Equals(Path.GetFileName(file)))
+                        {
+                            files.Add(file);
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    });
+                });
+            } catch (Exception ex)
+            {
+                return Enumerable.Empty<string>();
+            }
+
+            return files;
+
+            /*            if (searchOpt == SearchOption.TopDirectoryOnly)
+                        {
+                            List<string> T = new List<string>() { };
+                            Parallel.ForEach(Directory.EnumerateFiles(path, searchPattern, SearchOption.TopDirectoryOnly), file_found =>
+                                Parallel.For(0, dead_tracks.Count, index =>
+                                {
+                                    if (dead_tracks[index].Equals(Path.GetFileName(file_found)))
+                                    {
+                                        T.Add(file_found);
+                                    }
+                                }));
+                            return T;
+                        }*/
+
+            /*List<string> folders = new List<string>() { path };
+            int folCount = 1;
+            List<string> files = new List<string>() { };
+
+            try
+            {   //for each folder...
+                Parallel.For(0, folCount, index =>
+                {                                       
+                    try
+                    {
+                        //for each sub folder...
+                        Parallel.ForEach(Directory.EnumerateDirectories(folders[index], "*", SearchOption.TopDirectoryOnly), newDir =>
+                        {
+                            try
+                            {
+                                folders.Add(newDir);
+                                Debug.Print("will search directory " + newDir);
+                                folCount++;
+                                Debug.Print("folder count is " + folCount);
+
+                                //for each file in each folder
+                                Parallel.ForEach(Directory.EnumerateFiles(newDir, searchPattern), file =>
+                                {                                   
+                                    try
+                                    {   
+                                        //for each dead track
+                                        Parallel.For(0, dead_tracks.Count, index2 =>
+                                        {
+                                            //Debug.Print("searching directory " + newDir + " for track " + dead_tracks[index2]);
+                                            if (dead_tracks[index2].Equals(Path.GetFileName(file)))
+                                            {
+                                                Debug.Print("track matched");
+                                                files.Add(file);
+                                            }
+                                            else
+                                            {
+                                                //continue;
+                                                //Debug.Print("track did not match");
+                                                return;
+                                            }
+                                        });
+                                        return;
+                                    } catch (Exception ex)
+                                    {
+                                        Debug.Print("Ex when matching tracks");
+                                        return;
+                                    }
+                                });
+                            }
+                            catch (Exception ex)
+                            {
+                                Debug.Print("Ex when searching files");
+                                return;
+                            }
+                            return;
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.Print("this was the last exception before it broke " + ex);
+                        return;
+                    }
+                    return;
+                });
+            }
+            catch (Exception ex)
+            {
+                Debug.Print("Ex when searching folders");
+                return Enumerable.Empty<string>();
+            }*/
+
+            //return files;
+        }
+    }
+}
